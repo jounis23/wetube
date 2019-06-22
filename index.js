16891ea7dfe28d61312b48c6df06b2fb2ac3c0b5
Nagle <dydgml1935@gmail.com>
2019-06-22 22:16:59 +0900

    cookieparser, bodyparser, helmet, morgan을 이용하였다. 주석참고

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -9,12 +14,14 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res)=> res.send("You are on my profile");
 
-const betweenHome = (req, res, next) =>{
-    console.log("Between");
-    next();
-};
-
-app.use(betweenHome);
+app.use(cookieParser());
+//bodyparser -> 서버가 데이터 형식을 이해 하도록 돕는 것
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+//helmet -> 보안 그냥 쓰면 좋음
+app.use(helmet());
+//morgan -> 로그를 띄어줘서 여러가지 정보를 제공
+app.use(morgan("dev"));
 
 app.get("/", handleHome);
 
