1b428c8cb89332276c9d6d9a4328d0625fd8cace
Nagle <dydgml1935@naver.com>
2019-06-22 18:32:26 +0900

    프로젝트를 실행할때 쓰이는게 아니라 개발자에게만 효과를 주기위해 옵션 사용 가능 ex) npm install nodemon -D nodemon을 사용하면 저장할때마다 서버를 초기화 시켜줌 babel을 이용하여 쉽게 알아볼수 있도록 문볍변환을 시켜줌 arrowfuntion을 이용하면 함수를 이쁘게 만들 수 잇음 ES6버전의 기능 중 하나

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
+//arrow function
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
 
-function handleListening(){
-    console.log(`Listening on : http://localhost:${PORT}`);
-}
-function handleHome(req, res){
-    res.send("Hello from home");
-}
-function handleProfile(req, res){
-    res.send("You are on my profile");
-}
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res)=> res.send("You are on my profile");
 
 app.get("/", handleHome);
 
