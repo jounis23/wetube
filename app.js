b8b11e1ecc707a7c8dd4e85f73c3b1ca98ed553e
Nagle <dydgml1935@gmail.com>
2019-06-23 18:21:04 +0900

    MVC의 형태로 변화 M Model -> data V View -> how does the data look C Control -> function that looks for the data

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from home");
-
-const handleProfile = (req, res)=> res.send("You are on my profile");
 
 app.use(cookieParser());
 //bodyparser -> 서버가 데이터 형식을 이해 하도록 돕는 것
@@ -19,10 +17,8 @@ app.use(helmet());
 //morgan -> 로그를 띄어줘서 여러가지 정보를 제공
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("video", videoRouter);
 //누가 이걸 임포트 할때 app object를 가져간다
 export default app;
