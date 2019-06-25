54259357592fce3be76555b1f2381eaae235e25c
Nagle <dydgml1935@gmail.com>
2019-06-25 22:57:35 +0900

    localsMiddleware를 이용하여 local 변수에 접근 gobal이라고 하는거보다 local이라고 말해줘야됨

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,14 +12,16 @@ const app = express();
 
 app.set("view engine", "pug");
 
+//helmet -> 보안 그냥 쓰면 좋음
+app.use(helmet());
 app.use(cookieParser());
 //bodyparser -> 서버가 데이터 형식을 이해 하도록 돕는 것
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
-//helmet -> 보안 그냥 쓰면 좋음
-app.use(helmet());
 //morgan -> 로그를 띄어줘서 여러가지 정보를 제공
 app.use(morgan("dev"));
+app.use(localsMiddleware);
+
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
