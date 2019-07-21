6f6c842090d37a79d589d19573cad14fb0fc6f5e
Nagle <dydgml1935@gmail.com>
2019-07-22 00:58:26 +0900

    프론트엔드의 webpack 이용 및 설치 웹팩을 이용하면 여러가지의 최신 언어로 짜여있는 스크립트를 옛날형식으로 바꾸어주어 모든 브라우저에 호환이 가능하도록 한다.

--- models/Video.js
+++ models/Video.js
@@ -1,33 +1,31 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-    fileUrl: 
+  fileUrl: {
+    type: String,
+    require: "File URL is required"
+  },
+  title: {
+    type: String,
+    required: "Title is required"
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  comment: [
     {
-        type: String,
-        require: "File URL is required"
-    },
-    title: 
-    {
-        type: String,
-        required: "Title is required"
-    },
-    description: String,
-    views: 
-    {
-        type: Number,
-        default: 0
-    },
-    createdAt:
-    {
-        type: Date,
-        default: Date.now
-    },
-    comment:[{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Comment"
-    }]
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
 });
 
-const model = mongoose.model("Video", VideoSchema)
+const model = mongoose.model("Video", VideoSchema);
 
 export default model;
