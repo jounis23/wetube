c7293e250ccce3fcc0d65256a8fd343af140228b
Nagle <dydgml1935@gmail.com>
2019-07-24 23:54:18 +0900

    회원가입 passport-local-mongoose와 passport passport-local를 설치

--- middlewares.js
+++ middlewares.js
@@ -1,16 +1,16 @@
 import routes from "./routes";
 import multer from "multer";
 
-const multerVideo = multer({ dest : "uploads/videos/"});
+const multerVideo = multer({ dest: "uploads/videos/" });
 
-export const localsMiddleware = (req, res, next) =>{
-    res.locals.siteName = "WeTube";
-    res.locals.routes = routes;
-    res.locals.user ={
-        isAuthenticated: true,
-        id: 5
-    };
-    next();
-}
+export const localsMiddleware = (req, res, next) => {
+  res.locals.siteName = "WeTube";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 5
+  };
+  next();
+};
 
-export const uploadVideo = multerVideo.single("videoFile")
+export const uploadVideo = multerVideo.single("videoFile");
