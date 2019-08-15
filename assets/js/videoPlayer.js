108b850ee9ac8a7f068014fc75a96a827d9cbd44
Nagle <dydgml1935@gmail.com>
2019-08-16 00:27:50 +0900

    비디오 플레이어 볼륨 함수 설정

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -6,14 +6,26 @@ const volumeBtn = document.getElementById("jsVolumeBtn");
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   } else {
     videoPlayer.pause();
-    //playBtn.innerHTML =
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  }
+}
+
+function handleVolumeClick() {
+  if (videoPlayer.volume === 1) {
+    videoPlayer.volume = 0;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-off"></i>';
+  } else {
+    videoPlayer.volume = 1;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
   }
 }
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
